818a9fe8a74413142b415ebc2e854ecac9db4445
Max Smiley <max.smiley@tufts.edu>
2013-11-13 14:40:23 -0500

    renamed meow.js to whiskers.js and created tailjs

--- scripts/tail.js
+++ scripts/tail.js
@@ -1,9 +1,10 @@
 //Backend code for server / database
 var projects = [];
 var maxClipsStackSize = 10;
+
 //Project Data Structure
 
-
+//Add clips to project clips stack
 function updateProject(clips_data_JSON){
 	var clips_data = JSON.parse(jsonString);
 	projects[clips_data.project_id].clips_stack.unshift(clips_data.clips);
