9858ff0ee39e4b1e45ecfb3be07e327d5cf3b2d3
adempsey <amd0331@gmail.com>
2013-11-17 16:50:55 -0500

    added newproject endpoint

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser());
+
+var mongourl = process.env.MONGOHQ_URL;
+var db = require('mongodb').Db.connect(mongourl, function(error, dbConnection) { db=dbConnection; });
 
 app.get('/', function(request, response) {
 	response.send('meeeeeeeeowwwww');
+});
+
+app.post('/newproject', function(req, res) {
+	db.collection("projects", function(err, collection) {
+		collection.insert( {
+			name: req.body["name"],
+			created_at: (new Date()).toString(),
+			clips: []
+		}, function(err, inserted) {
+			if (err) {
+				console.log(err);
+				res.send(400);
+			} else {
+				res.send(200);
+			}
+		});
+	});
 });
 
 var port = process.env.PORT || 5000;
