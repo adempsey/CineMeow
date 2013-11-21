335a97d3d75cb36736af1fddfb240c5607f047da
Max Smiley <max.smiley@tufts.edu>
2013-11-21 14:23:59 -0500

    attempting updating values from drag and resize

--- public/assets/scripts/whiskers.js
+++ public/assets/scripts/whiskers.js
@@ -186,10 +186,10 @@ $(function () {
                 //$(this).text("");
                 $("#info").text("start:" + start + " end: " + (start+width) + " length: "+ width);
                 var idnum2 = $(this).attr('id');//.substring(5);//"drag"
-                var subst = idnum2.substring(4);//"drag"
-                console.log("IDNUM " + idnum2);
-                 console.log("ID " + subst);
-                //$("#start" + i).val();
+                var idnum = idnum2.substring(4);//"drag"
+
+                $("#start" + idnum).val(start);
+                $("#end" + idnum).val((start+width));
             } );
             $(".clip").bind("drag", function(e){
                 var position = $(this).offset();
