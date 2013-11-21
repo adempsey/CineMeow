78284c24c7d7b6910e5c637412405ddffde47a22
Max Smiley <max.smiley@tufts.edu>
2013-11-20 23:00:20 -0500

    attempting scaling factor

--- public/assets/scripts/whiskers.js
+++ public/assets/scripts/whiskers.js
@@ -238,16 +238,16 @@ $(function () {
             }); 
             $(".clip").resize(function(e){
                 var position = $(this).offset();
-                var start = 1.0 * (position.left - $("#drag-x").position.left)/scalingFactor; // TODO
-                var width = 1.0 * $(this).width() / scalingFactor;
+                var start = (position.left - $("#drag-x").position.left)/scalingFactor; // TODO
+                var width = $(this).width() / scalingFactor;
                 //e.stopPropagation();
                 //$(this).text("");
                 $(info).text("start:" + start + " end: " + (start+width) + " length: "+ width);
             } );
             $(".clip").bind("drag", function(e){
               var position = $(this).offset();
-                var start = 1.0 * (position.left - $("#drag-x").position.left)/scalingFactor; // TODO
-                var width = 1.0 * $(this).width() / scalingFactor;
+                var start = (position.left - $("#drag-x").position.left)/scalingFactor; // TODO
+                var width = $(this).width() / scalingFactor;
                 //e.stopPropagation();
                 //$(this).text("");
                 $(info).text("start:" + start + " end: " + (start+width) + " length: "+ width);
