d5b3d9a7dfa8fb8dcf3a88ceb0f14ef90d7d0ea5
Max Smiley <max.smiley@tufts.edu>
2013-12-10 21:52:22 -0500

    working on drags

--- public/assets/scripts/whiskers.js
+++ public/assets/scripts/whiskers.js
@@ -1,14 +1,15 @@
 //Adds video at correct location, and generates an ID for it?
 function addClipToData(clip){
 	//Insert clip in chronological index\
-	for(var i = 0; i < clips_data.length; i ++){
-		if(clips_data[i].timeline_start_time > clip.timeline_start_time){
-			clips_data.splice(i, 0, clip);
-			inserted = true;
+	for(var i = 0; i < project.clips.length; i ++){
+		if(project.clips[i].timeline_start_time > clip.timeline_start_time){
+			project.clips.splice(i, 0, clip);
 			return;
 		}
 	}
-	clips_data.push(clip);
+    for(var i = 0; i < project.clips.length; i ++){
+        project.clips[i]["clip_id"] = i;
+    }
 }
 
 //If clip is updated, its time might change, so remove it and reinsert it into the array 
@@ -160,9 +161,10 @@ function requestPause() {
 var rtime = new Date(1, 1, 2000, 12,00,00);
 var timeout = false;
 var delta = 200;
+ var scalingFactor = 10;
 function addClipToTimeline(i,color){
     console.log(project.clips.length);
-    var scalingFactor = 10;
+   
     var timelineid = "#drag-x";
     var clip=project.clips[i];
     var color="#"+Math.floor(((1/(1+clip["clip_id"]))*7216)+15770000).toString(16); // lol
@@ -334,80 +336,8 @@ function saveClips(update_stack, message) {
 //Video Clips Menu
 
 $(function(){
-    /*for (var i in project.clips) {
-        var clip=project.clips[i];
-        var color="#"+Math.floor((Math.random()*7216)+15770000).toString(16); // lol
-        $("#drag-clipsviewer").append('<div id="dragclip'+i+'" class="drag clip" style="background-color:'+color+'">'+clip.name+'</div>');
-    }
-    for (var i in project.clips) {
-        $("#dragclip"+i).offset({left: project.clips[i]["start_time"]*scalingFactor + $("#drag-clipsviewer").offset().left} );
-        $("#dragclip"+i).width((project.clips[i]["end_time"]-project.clips[i]["start_time"])*scalingFactor);
-        console.log("width " + (project.clips[i]["end_time"]-project.clips[i]["start_time"])*scalingFactor);
-    }*/
     var container_count = 2;
-    /*
-    //HARDCODED:
-      $("#drag-clipsviewer").append('<table id="clipsource'+0+'"  class= "clip_source" style=" width: 530px, background-color: black"> </table>');
-      $("#drag-clipsviewer").append('<table id="clipsource'+1+'"  class= "clip_source" style=" width: 530px, background-color: black"> </table>');
-      $("#clipsource" +0).append('<tr> <td> <div id="dragclone'+0+'"  class="drag_clone" > drag </div> </td>'
-                                       +'<td> <div id="clipcontainer'+0+'" class="clip_container" style="background-color: E0F0FF"> </div> </td>'
-                                 +'</tr>');
-      $("#clipsource" +1).append('<tr> <td> <div id="dragclone'+1+'"  class="drag_clone" > drag </div> </td>'
-                                       +' <td> <div id="clipcontainer'+1+'" class="clip_container" style="background-color: E0F0FF"> </div> </td>'
-                                  +' </tr>');
-      var color="#"+Math.floor((Math.random()*7216)+15770000).toString(16); // lol
-      $("#clipcontainer" +0).append('<div id="dragclip'+0+'" class="dragclip drag" style="background-color:'+color+'"> some clip </div>');
-      var color="#"+Math.floor((Math.random()*7216)+15770000).toString(16); // lol
-      $("#clipcontainer" +1).append('<div id="dragclip'+1+'" class="dragclip drag" style="background-color:'+color+'"> some other clip </div>');
-      */
     retrieveVideos();
-
-    /* X axis only */
-    /*for(var i = 0; i < container_count; i ++){
-        $("#dragclip"+i).draggable({
-                containment: "#clipcontainer" + i,
-                stack: ".dragclip",
-                axis: "x",
-                grid: [1,1],  
-                snap: true,
-                snapTolerance: 5, 
-                stop: function() {
-                    //console.log("saving clips!");
-                    //saveClips();
-                }
-                //,
-                //drag: function(e){
-                //}
-        });
-        $("#dragclip"+i).resizable({
-            handles: 'e, w', 
-            minWidth: 10, //maxwidth will be determined by video clip!
-            minHeight: 70,
-            maxWidth: 500,
-            //containment: "#clipcontainer" + i,
-            //TODO GET CONTAINMENT WORKING DYNAMICALLY
-            //containment: "#drag-clipsviewer"//containment: "#clipcontainer"+i
-        });
-    }*/
-
-     $( ".drag_clone" ).draggable({
-        helper: function(event) {
-            var clone = $(event.target).clone();
-            clone.removeClass(".drag_clone");
-            clone.addClass(".drag");
-            clone.css({ "background-color": "orange", //TODO: color of clip in viewer 
-                        "width": "75px",
-                        "height": "75px",
-                        "minWidth": "75px",
-                        "minHeight": "75px",
-                        "border-radius":"8px",
-                        "opacity": ".7",
-                        "border":"none" });
-            return clone;
-        },
-        revert: "invalid",
-    });
-
     $( "#drag-x").droppable({
             accept: ".drag_clone",
             activeClass: "ui-state-hover",
@@ -452,13 +382,15 @@ function retrieveVideos() {
                     $("#drag-clipsviewer").append('</tr>');
                     cliprow++;
                 }
+                duration = $("#eyebrows")[0].duration*scalingFactor + "px";
+                console.log("DURATION " + duration);
                 $( "#dragclone" +i).draggable({
                     helper: function(event) {
                         var clone = $(event.target).clone();
                         clone.removeClass(".drag_clone");
                         clone.addClass(".drag");
                         clone.css({ "background-color": "orange", //TODO: color of clip in viewer 
-                                    "width": "75px",
+                                    "width": duration,
                                     "height": "75px",
                                     "minWidth": "75px",
                                     "minHeight": "75px",
